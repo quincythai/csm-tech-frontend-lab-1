8394fb7b830e639be7a9fa92e0b4aaee9b165fd0
Noor Mahini <nmahini@berkeley.edu>
2022-06-05 15:02:17 -0700

    updated document title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import {useEffect} from 'react';
 import Resources from './components/Resources.js'
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    document.title = "CSM Tech Lab 1";
+  }, [])
+
   return (
     <div className="App">
       <Resources />
     </div>
   );
+
+
 }
 
 export default App;
